c2391f4214277b24bd2d6ff69e50d9abbae9c89f
Devansh Lodhi <1129.gh0st@gmail.com>
2023-06-24 20:36:37 +0530

    redux

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,18 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const runtime = 'edge'
-
-export const metadata = {
-  title: 'NxtApp',
-  description: 'Nxt',
-}
+export const runtime = "edge";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <Provider store={store}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </Provider>
+  );
 }
